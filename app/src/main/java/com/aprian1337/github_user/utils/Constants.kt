83934cdad6a14c3a70923ccbfc369cb8720aac53
aprian1337 <dwikyapriyan@gmail.com>
2021-04-13 23:26:11 +0700

    adding constants data

--- app/src/main/java/com/aprian1337/github_user/utils/Constants.kt
+++ app/src/main/java/com/aprian1337/github_user/utils/Constants.kt
@@ -10,5 +10,10 @@ class Constants {
         const val ENDPOINT_DETAIL_FOLLOWERS = "/users/{username}/followers"
         const val ENDPOINT_DETAIL_FOLLOWING = "/users/{username}/following"
         const val AUTH_TOKEN = BuildConfig.AUTH_TOKEN
+        const val TABLE_FAVORITE = "fav_user"
+        const val HOUR_TIME = 9
+        const val MINUTE_TIME = 0
+        const val SECOND_TIME = 0
+        const val ID_REPEATING = 101
     }
 }
